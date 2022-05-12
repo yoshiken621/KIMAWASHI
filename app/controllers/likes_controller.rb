d2be36e679af4d7ab26fa1262ab458213a078ff7
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-05-12 10:24:44 +0900

    like controllerの可読性の強化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,19 @@
 class LikesController < ApplicationController
   before_action :event_params
 
+  # いいねの個数の追加処理
   def create
     Like.create(user_id: current_user.id, event_id: params[:id])
   end
 
+  # いいねの個数の削除処理
   def destroy
     Like.find_by(user_id: current_user.id, event_id: params[:id]).destroy
   end
 
   private
 
+  # いいねを付与する先のアイテムの指定
   def event_params
     @event = Event.find(params[:id])
   end
