01dfaf35a73f3fba3169a6e979a1bebd4984ac4b
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-05-12 10:17:10 +0900

    application controllerの可読性の強化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  # ログインや新規登録する際に受け取る情報の選別の前処理
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
+  # ログインや新規登録の際にデータベースに送られる情報の内訳
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :age_select_id, :sex_select_id, :email])
   end
