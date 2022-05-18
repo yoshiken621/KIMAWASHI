a4a0025529792dadced888d91a31ff91e68a52bb
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-05-18 10:52:32 +0900

    カレンダーの表示詳細の設定を行うファイルの可読性の強化

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -1,15 +1,20 @@
+// フルカレンダーの機能の読み込み
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
+// カレンダー要素を呼び出す処理
 document.addEventListener('turbolinks:load', function() {
   var calendarEl = document.getElementById('calendar');
 
   const calendar = new Calendar(calendarEl, {
     plugins: [ dayGridPlugin, interactionPlugin],
+    // 日本語化
     locale: 'ja',
+    // 日本時間への変更
     timeZone: 'Asia/Tokyo',
     firstDay: 1,
+    // 左上のボタンへのリンクの付与
     customButtons: {
       myCustomButton: {
         text: 'みんなの投稿',
@@ -18,6 +23,7 @@ document.addEventListener('turbolinks:load', function() {
         }
       }
     },
+    // カレンダー上部の表示内容
     headerToolbar: {
       start: 'myCustomButton',
       center: 'title',
@@ -27,7 +33,7 @@ document.addEventListener('turbolinks:load', function() {
       today: '今日',
       month: '月',
     },
-
+    // イベント情報のデータベースからの取得
     events: '/events.json',
     
 
@@ -36,14 +42,18 @@ document.addEventListener('turbolinks:load', function() {
     dayCellContent: function(e) {
       e.dayNumberText = e.dayNumberText.replace('日', '');
     },
+    // カレンダーの日付部分をクリックしたときにその日付情報を取得して投稿ページに送る処理
     dateClick: function(e){ 
       location.href = `/events/new?date=${e.dateStr}` ;
     },
+    // イベントをクリックしたときにイベントの詳細情報ページへの遷移
     eventClick: function(e){
       location.href = `/events/${e.event.id}`
     },
 
+    // イベントの表示
     eventContent: function(e) {
+      // イベントのタイトルの表示
       let arrayOfDomNodes = []
       let titleEvent = document.createElement('div')
       if(e.event._def.title) {
@@ -51,6 +61,7 @@ document.addEventListener('turbolinks:load', function() {
         titleEvent.classList = "fc-event-title fc-sticky"
       }
 
+      // イベントの画像を表示
       let imgEventWrap = document.createElement('div')
       if(e.event.extendedProps.image_url) {
         let imgEvent = '<img src="'+e.event.extendedProps.image_url+'" >'
@@ -62,6 +73,7 @@ document.addEventListener('turbolinks:load', function() {
 
       return { domNodes: arrayOfDomNodes }
     },
+    // マウスをイベント上に置いた時にイベントの詳細を表示する処理
     eventDidMount: function(info) {
       tippy(info.el, {
          content: info.event.extendedProps.description,
