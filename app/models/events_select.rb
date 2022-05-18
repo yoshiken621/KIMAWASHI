da0fe6c3f170a7f3db6a57fdc38ff8603bbdbb98
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-05-18 12:55:13 +0900

    その日の出来事の情報の選択肢を表示するためのアクティブハッシュの可読性の強化

--- app/models/events_select.rb
+++ app/models/events_select.rb
@@ -1,3 +1,4 @@
+# その日の出来事の情報の選択肢を表示するためのアクティブハッシュ
 class EventsSelect < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
@@ -7,6 +8,7 @@ class EventsSelect < ActiveHash::Base
     { id: 5, name: 'その他' }
   ]
 
+  # イベントモデルとの紐付け
   include ActiveHash::Associations
   has_many :events
 end
