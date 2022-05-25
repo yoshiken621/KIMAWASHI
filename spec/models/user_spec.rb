b6e33e6085478d36596ca2d8e2806c6dedcde2f9
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-05-25 18:27:15 +0900

    テストコードファクトリーボットの導入まで

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+    end 
+    it 'age_select_idが空では登録できない' do
+    end
+    it 'sex_select_idが空では登録できない' do
+end
