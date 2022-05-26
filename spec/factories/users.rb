44b505dd1900ba92399814657daad82dee3331f3
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-05-26 14:59:04 +0900

    ユーザー登録ができる場合のテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    nickname       {'ken'}
+    age_select_id  { 2 } 
+    sex_select_id  { 2 }
+    email          {'a@a'}
+    password  {'oooooo'}
+    password_confirmation {password}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build (:user)
+  end 
+
   describe 'ユーザー新規登録' do
-    it 'nicknameが空では登録できない' do
+    context '新規登録できる場合' do
+      it 'nickname, age_select_id, sex_select_id, email, password, password_confirmationが存在する' do
+        expect(@user).to be_valid
+      end 
     end 
-    it 'age_select_idが空では登録できない' do
-    end
-    it 'sex_select_idが空では登録できない' do
+
+    context '新規登録できない場合' do
+      it 'nicknameが空では登録できない' do
+      end 
+      it 'age_select_idが空では登録できない' do
+      end
+      it 'sex_select_idが空では登録できない' do
+      end 
+    end 
+  end 
 end
