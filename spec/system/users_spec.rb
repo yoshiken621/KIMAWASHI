288bc7919fce8ad31d03daf53d2cfff8427ad7f5
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-05-31 18:05:06 +0900

    結合テストユーザー新規登録

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "ユーザー新規登録", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  # context 'ユーザー新規登録ができるとき' do 
+  #   it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+  #     visit new_user_registration_path
+  #     fill_in 'Nickname', with: @user.nickname
+  #     select '10代', from: 'user[age_select_id]'
+  #     select '男性', from: 'user[sex_select_id]'
+  #     fill_in 'Email', with: @user.email
+  #     fill_in 'Password', with: @user.password
+  #     fill_in 'Password confirmation', with: @user.password_confirmation
+  #     expect{find('input[value="登録"]').click}.to change { User.count }.by(1)
+  #     expect(current_path).to eq(root_path)
+  #   end
+  # end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      visit new_user_registration_path
+      fill_in 'Nickname', with: ''
+      select '--', from: 'user[age_select_id]'
+      select '--', from: 'user[sex_select_id]'
+      fill_in 'Email', with: ''
+      fill_in 'Password', with: ''
+      fill_in 'Password confirmation', with: ''
+      expect{find('input[value="登録"]').click}.to change { User.count }.by(0)
+      expect(current_path).to eq(user_registration_path)
+    end
+  end
+end
