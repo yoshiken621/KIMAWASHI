e9e6945f99c14a6f7c9d1a43a8b6d309906bc1b5
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-06-03 18:27:55 +0900

    FactoryBot日付けの設定変更

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :event do
-    date {Faker::Date.between(from: '2014-09-23', to: '2014-09-25')}
+    date {'2014-09-25'}
     events_select_id { 2 }
     met_person { 'aaa' }
     checkbox { 1 }
