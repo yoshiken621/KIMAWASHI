54b7bbafe255d38ed6734eb1f4c2111decd01e33
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-06-03 18:27:20 +0900

    Fakerの導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname       {'ken'}
+    nickname       {Faker::Name.initials(number: 2)}
     age_select_id  { 2 } 
     sex_select_id  { 2 }
-    email          {'a@a'}
+    email          {Faker::Internet.free_email}
     password  {'oooooo'}
     password_confirmation {password}
   end
